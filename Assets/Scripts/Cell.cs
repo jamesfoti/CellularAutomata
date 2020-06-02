df187f0ef06fd3c9322983f3d315115c6a9a155e
James Foti <jameswilliamfoti@gmail.com>
2020-06-02 01:39:50 -0700

    Initial commit.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Cell : MonoBehaviour {
+
+	[HideInInspector]
+	public bool isAlive;
+	public Material material;
+	public float neighborRadius = 1f;
+
+	private void Awake() {
+		this.material = GetComponent<Renderer>().material;
+	}
+
+	private void Update() {
+		// CheckNeighbors()
+	}
+
+	private void CheckNeighbors() {
+		// GetNearbyCells()
+		int numAliveCells = CountNearbyLiveCells();
+
+		/* Rules:
+		 * 1) Any cell with fewer than 2 live neighbors dies -> underpopulation
+		 * 2) Any cell with 2 or 3 neighbors lives on to the next generation
+		 * 3) Any cell with more than 3 neighbors dies -> overcrowding
+		 * 4) Any dead cell with exactly 3 live neighbors comes back to life -> reproduction
+		 */
+
+		if (this.tag == "isAlive") {
+			UnderPopulation(numAliveCells);
+			Lives(numAliveCells);
+			OverCrowding(numAliveCells);
+		}
+		else if (this.tag == "isDead") {
+			Reproduction(numAliveCells);
+		}
+	}
+
+	private int CountNearbyLiveCells() {
+		Collider2D[] neighborCells = Physics2D.OverlapCircleAll(this.transform.position, neighborRadius);
+
+		int numAliveCells = 0;
+
+		foreach (Collider2D cellCollider in neighborCells) {
+			if (cellCollider.tag == "isAlive") {
+				numAliveCells++;
+			}
+		}
+
+		return numAliveCells;
+
+
+	}
+
+	private void UnderPopulation(int numAliveCells) {
+
+		if (numAliveCells < 2) {
+			this.tag = "isDead";
+			this.material.color = Color.white;
+		}
+	}
+
+	private void Lives(int numAlivecells) {
+		
+		if (numAlivecells == 2 || numAlivecells == 3) {
+			this.tag = "isAlive";
+			this.material.color = Color.green;
+		}
+	}
+
+	private void OverCrowding(int numAliveCells) {
+
+		if (numAliveCells > 3) {
+			this.tag = "isDead";
+			this.material.color = Color.white;
+		}
+	}
+
+	private void Reproduction(int numAlivecells) {
+		
+		if (numAlivecells == 3) {
+			this.tag = "isAlive";
+			this.material.color = Color.green;
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour {
+
+	public Cell cell;
+
+	[SerializeField]
+	private int numRows;
+	[SerializeField]
+	private int numCols;
+	[SerializeField]
+	private float tileSize;
+	[SerializeField]
+	private float distanceOffset;
+
+	private Color[] colors;
+
+	private void Start() {
+
+		colors = new Color[2];
+		colors[0] = Color.white;
+		colors[1] = Color.green;
+
+		GenerateGrid();
+	}
+
+	private void GenerateGrid() {
+		for (int row = 0; row < numRows; row++) {
+			for (int col = 0; col < numCols; col++) {
+
+				float xCord = col * tileSize;
+				float yCord = row * -tileSize;
+				Vector2 cellPosition = new Vector2(xCord, yCord);
+
+				Cell cell = Instantiate(this.cell, parent: this.transform);
+				cell.transform.localPosition = cellPosition;
+				cell.material.color = Color.red;
+				cell.transform.localScale = new Vector3(tileSize - distanceOffset, tileSize - distanceOffset, tileSize - distanceOffset);
+			}
+		}
+
+		float gridWidth = numCols * tileSize;
+		float gridHeight = numRows * tileSize;
+		this.transform.position = new Vector2(-gridWidth / 2 + tileSize / 2, gridHeight / 2 - tileSize / 2);
+	}
+
+}
