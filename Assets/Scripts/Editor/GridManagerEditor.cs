49a3ebd97ec6be2b8c911b21ad1697ab1d43d0a7
James Foti <jameswilliamfoti@gmail.com>
2020-08-30 11:40:23 -0700

    Update

---
+++ Assets/Scripts/Editor/GridManagerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GridManager))]
+public class GridManagerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		GridManager gridManager = (GridManager)target;
+
+		if (DrawDefaultInspector()) {
+			if (gridManager.autoUpdate) {
+				gridManager.GenerateGrid();
+			}
+		}
+
+		if (GUILayout.Button("Generate Grid")) {
+			gridManager.GenerateGrid();
+		}
+
+		if (GUILayout.Button("Clear Grid")) {
+			gridManager.ClearGrid();
+		}
+	}
+}
